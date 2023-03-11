4fbf9c54a0c36f4b09f141ab79e86e2982cf7628
Viviana Vilchis <80860348+vivianavilchis01@users.noreply.github.com>
2023-03-10 23:18:46 -0600

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('A00826414 Viviana Vilchis - App de Netflix')
+st.title('A00829263 Kelvin Lopez - App de Netflix')
 
 DATE_COLUMN = 'released'
 DATA_URL = ('movies.csv')
